309410ff4ae3dba43fb312fbcc63cf18aa9843b0
MuhammadKhomaini <muhkhomaini22@gmail.com>
2025-06-15 23:47:35 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -97,3 +97,61 @@ struct BattleState {
 int main(){
 
 }
+vector<Weapon> weapons = {
+    {"Sword", 0, 0, 0},
+    {"Bow", 0, 0, 0},
+    {"Wand", 0, 0, 0},
+    {"Hammer", 0, 0, 0}
+};
+
+vector<Rune> allRunes = {
+    {"Titan Core", 1200, 150, 100, 3},
+    {"Berserker", 800, 450, 100, 3},
+    {"Iron Wall", 1000, 100, 250, 3},
+    {"Balanced", 600, 150, 150, 2},
+    {"Charger", 400, 250, 50, 2},
+    {"Guard", 700, 100, 100, 2},
+    {"Assassin", 300, 300, 30, 2},
+    {"Sentinel", 750, 50, 180, 2},
+    {"Hybrid", 550, 180, 80, 2},
+    {"Swift", 200, 120, 30, 1},
+    {"Bulwark", 300, 60, 60, 1},
+    {"Ember", 150, 150, 30, 1},
+    {"Core", 250, 80, 40, 1},
+    {"Shield", 280, 40, 70, 1},
+    {"Strike", 180, 130, 20, 1},
+    {"Vigor", 220, 110, 30, 1},
+    {"Sprint", 260, 90, 50, 1},
+    {"Piercer", 160, 140, 25, 1},
+};
+
+
+int inputInt(int min, int max) {
+    int val;
+    while (true) {
+        cin >> val;
+        if (!cin.fail() && val >= min && val <= max) break;
+        cout << "Invalid input. Try again: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    return val;
+}
+
+
+double maxPower = 0;
+void knapsack(int idx, int val, vector<Rune> &curr, vector<Rune> &opt) {
+    if (val > 7) return;
+    double hp = 0, atk = 0, def = 0;
+    for (auto &r : curr) hp += r.hp, atk += r.atk, def += r.def;
+    double p = (atk / 2.0) + (hp / 10.0) + (def / 7.0);
+    if (p > maxPower) {
+        maxPower = p;
+        opt = curr;
+    }
+    for (int i = idx; i < allRunes.size(); ++i) {
+        curr.push_back(allRunes[i]);
+        knapsack(i + 1, val + allRunes[i].value, curr, opt);
+        curr.pop_back();
+    }
+}
